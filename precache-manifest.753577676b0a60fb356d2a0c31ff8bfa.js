64decb6c4c1bca2a174dd35540897a6c52cff1cf
Stef Havermans <stef.havermans@gmail.com>
2020-10-09 15:04:56 +0200

    Update asset-manifest.json, favicon.ico, and 10 more files...

---
+++ precache-manifest.753577676b0a60fb356d2a0c31ff8bfa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e97b980e57693f68c1d738214fdcbf5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d45664d43a8f4b4fe79",
+    "url": "/static/js/2.2f60f30a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2f60f30a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcb8af8850bb1f8b9d56",
+    "url": "/static/js/main.de721172.chunk.js"
+  },
+  {
+    "revision": "255b1972d666b5158a06",
+    "url": "/static/js/runtime-main.d75985c5.js"
+  }
+]);
